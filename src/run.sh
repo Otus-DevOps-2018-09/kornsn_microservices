029938623b3a7651050026e532223e59a06c3e2c
Sergey Korneev <korneev@tn.ru>
2019-01-23 17:53:32 +0400

    HW Docker-4

--- src/run.sh
+++ src/run.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db --volume reddit_db:/data/db mongo
-docker run -d --network=reddit --network-alias=post kornsn/post:1.0
-docker run -d --network=reddit --network-alias=comment kornsn/comment:1.0
-docker run -d --network=reddit -p 9292:9292 kornsn/ui:1.0
+docker run -d --network=back-net --network-alias=post_db --network-alias=comment_db --volume reddit_db:/data/db --name db mongo
+docker run -d --network=back-net --network-alias=post --name post kornsn/post:1.0
+docker run -d --network=back-net --network-alias=comment --name comment kornsn/comment:1.0
+docker run -d --network=front-net -p 9292:9292 --name ui kornsn/ui:1.0
+
+docker network connect front-net post
+docker network connect front-net comment
